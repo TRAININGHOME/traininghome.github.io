cc86214b579be5a2391e6a4abd7760351df8d7bc
TRAININGHOME <milan.ewok@gmail.com>
2024-09-19 13:22:21 -0600

    primer version

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Al finalizar la animación, se oculta el preloader y se muestra el contenido
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    const content = document.getElementById('content');
+
+    // Después de la animación de fadeOut, ocultar el preloader y mostrar el contenido
+    preloader.addEventListener('animationend', () => {
+        preloader.style.display = 'none';
+        content.style.display = 'block';
+    });
+});
