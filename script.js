9b432ba3ccc2c0195eaca8260cd124453623b98f
TRAININGHOME <milan.ewok@gmail.com>
2024-09-20 13:22:58 -0600

    sera?
    si no ya valio

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ window.addEventListener('load', () => {
         content.style.display = 'block';
     });
 });
+// Mostrar la caja emergente cuando se cargue la página o al hacer clic en un botón
+window.addEventListener('load', () => {
+    const carouselPopup = document.querySelector('.carousel-popup');
+    carouselPopup.style.display = 'flex'; // Mostrar el popup al cargar la página
+});
+
+// También puedes añadir un botón de cierre en el HTML y JavaScript para ocultar el popup si es necesario.
