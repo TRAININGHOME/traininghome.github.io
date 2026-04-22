91ab94168cc07d4f430608441110ab9e66fa487a
Julio Milan <milan.ewok@gmail.com>
2026-04-22 17:01:13 -0600

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// Cambia el mensaje de bienvenida para que se vea profesional
-app.get('/', (req, res) => {
-  res.json({
-    status: "Operacional",
-    system: "MANTRA Core Engine",
-    client: "Hugo Isac - Management System",
-    version: "2.0.4"
-  });
-});
